c530d013f0c10e7d8d60fd084a010ddbf5e9a022
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-08-24 12:50:58 -0400

    Rewrote based on Jade's example so that this actually works as a plugin

--- collate_by_sub_collection.rb
+++ collate_by_sub_collection.rb
@@ -1,33 +1,35 @@
-## my-website/plugins/pages_find_by_name.rb
+## my-website/plugins/collate_by_sub_collection.rb
 
-def collate_by_sub_collection
-  collated = Hash.new
-  sub_collections = Hash.new
-  pages = all
-  pages.each_with_index do |page, i|
-    if page.pointer['id'].scan('/').length > 0
-      sub_collection = page.pointer['id'].split('/')
-      sub_collection.pop
-      
-      if sub_collections[sub_collection.join('/').to_sym].nil?
-        sub_collections[sub_collection.join('/').to_sym] = Array.new
+module CollateBySubCollection
+  def collate_by_sub_collection
+    collated = Hash.new
+    sub_collections = Hash.new
+    pages = all
+    pages.each_with_index do |page, i|
+      if page.pointer['id'].scan('/').length > 0
+        sub_collection = page.pointer['id'].split('/')
+        sub_collection.pop
+        
+        if sub_collections[sub_collection.join('/').to_sym].nil?
+          sub_collections[sub_collection.join('/').to_sym] = Array.new
+        end
+        sub_collections[sub_collection.join('/').to_sym] << page['id']
+      else
+        if sub_collections[:root].nil?
+          sub_collections[:root] = Array.new
+        end
+        sub_collections[:root] << page['id']
       end
-      sub_collections[sub_collection.join('/').to_sym] << page['id']
-    else
-      if sub_collections[:root].nil?
-        sub_collections[:root] = Array.new
-      end
-      sub_collections[:root] << page['id']
     end
+    sub_collections.each do |collection,posts|
+      collated[collection] = {
+        'directory' => collection,
+        resource_name => posts
+      }
+    end
+    collated["all"] = collated.each_value.map { |sub_collection| sub_collection }
+    collated
   end
-  sub_collections.each do |collection,posts|
-    collated[collection] = {
-      'directory' => collection,
-      resource_name => posts
-    }
-  end
-  collated["all"] = collated.each_value.map { |sub_collection| sub_collection }
-  collated
 end
 
-Ruhoh::Resources::Pages::CollectionView.send(:include, collate_by_sub_collection)
+Ruhoh::Resources::Pages::CollectionView.send(:include, CollateBySubCollection)
